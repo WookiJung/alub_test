75d2f5e7ac204bf182e91860d8c336d6dfcc30ce
WookiJung <77482837+WookiJung@users.noreply.github.com>
2021-11-10 11:09:53 +0900

    BOJ-16953 'A → B' [ 21/11/10 11:09:55 ] by Alub

---
+++ BOJ/16953/16953_7.py
@@ -0,0 +1,28 @@
+/*
+* 메모리: 29200 KB, 시간: 72 ms
+* 타이머 시간: null
+* 2021.11.10
+* by Alub
+**/
+def atob(b, a, cnt):
+    if b == a:
+        print(f'{cnt}')
+
+    if b > a:
+        if b % 10 == 1:
+            c = b // 10
+            cnt += 1
+            atob(c, a, cnt)
+        else:
+            c = b//2
+            if c * 2 == b:
+                cnt += 1
+                atob(c, a, cnt)
+            else:
+                print('-1')
+
+    if b < a:
+        print('-1')
+
+A, B = map(int, input().split())
+atob(B, A, 1)
