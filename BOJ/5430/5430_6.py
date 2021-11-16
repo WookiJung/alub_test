0eb740bccf1e0b1ed22acaa808ce12858eca0300
WookiJung <77482837+WookiJung@users.noreply.github.com>
2021-11-16 13:56:19 +0900

    BOJ-5430 'null' [ 21/11/16 13:56:19 ] by Alub

---
+++ BOJ/5430/5430_6.py
@@ -0,0 +1,33 @@
+/*
+* 메모리: 44160 KB, 시간: 184 ms
+* 타이머 시간: 02:44:14
+* 2021.11.16
+* by Alub
+**/
+for tc in range(int(input())):
+    p = input()
+    N = int(input())
+    array = input()
+    array_num = array[1:len(array) - 1].split(',')
+
+    p = p.replace('RR', '')
+
+    r = 0
+    s, e = 0, 0
+    for i in p:
+        if i == 'R':
+            r += 1
+        elif i == 'D':
+            if r % 2 == 0:
+                s += 1
+            else:
+                e += 1
+                
+    if s + e <= N:
+        result = array_num[s:N-e]
+        if r % 2:
+            print('[' + ','.join(result[::-1]) + ']')
+        else:
+            print('[' + ','.join(result) + ']')
+    else:
+        print('error')
