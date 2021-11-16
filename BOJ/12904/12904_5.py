d18e31eacc289ae6834997e2930cb8817dae31e2
WookiJung <77482837+WookiJung@users.noreply.github.com>
2021-11-16 16:31:51 +0900

    BOJ-12904 'A와 B' [ 21/11/16 16:31:52 ] by Alub

---
+++ BOJ/12904/12904_5.py
@@ -0,0 +1,30 @@
+/*
+* 메모리: 29200 KB, 시간: 76 ms
+* 타이머 시간: null
+* 2021.11.16
+* by Alub
+**/
+S = input()
+T = input()
+len_T = len(T)
+
+def atob(T):
+    global ans
+    for i in range(len_T - 1, len(S) - 1, -1):
+
+        if T[i] == 'A':
+            T = T[:len(T)-1]
+        elif T[i] == 'B':
+            T = T[len(T)-2::-1]
+
+    if T == S:
+        ans = 1
+    else:
+        ans = 0
+    return ans
+
+
+ans = 1
+
+atob(T)
+print(ans)
