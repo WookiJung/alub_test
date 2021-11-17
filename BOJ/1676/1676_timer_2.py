6e843e87d95e5c385ac73268295f1153d27f23a9
WookiJung <77482837+WookiJung@users.noreply.github.com>
2021-11-18 02:19:49 +0900

    BOJ-1676 '팩토리얼 0의 개수' [ 21/11/18 02:19:49 ] by Alub

---
+++ BOJ/1676/1676_timer_2.py
@@ -0,0 +1,17 @@
+/*
+* 메모리: 29200 KB, 시간: 68 ms
+* 타이머 시간: 00:00:37
+* 2021.11.18
+* by Alub
+**/
+N = int(input())
+result = 0
+for n in range(1,N+1):
+    if n % 5 == 0:
+        result += 1
+        if n % 25 == 0:
+            result += 1
+            if n % 125 == 0:
+                result += 1
+
+print(result)
