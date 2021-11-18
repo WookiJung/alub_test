638dc77a6d4ef5cdd049dc1262224bd3278ee7c6
WookiJung <77482837+WookiJung@users.noreply.github.com>
2021-11-18 10:42:46 +0900

    BOJ-20364 '부동산 다툼' [ 21/11/18 10:42:48 ] by Alub

---
+++ BOJ/20364/1.py
@@ -0,0 +1,26 @@
+/*
+* 메모리: 140340 KB, 시간: 352 ms
+* 타이머 시간: null
+* 2021.11.18
+* by Alub
+**/
+import sys
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+visited = [0] * (N+1)
+
+for i in range(Q):
+    x = int(input())
+    duck = x
+    found = True
+    while x > 0:
+        if visited[x]:
+            last_owned = x
+            found = False
+        x //= 2
+    if found:
+        visited[duck] = 1
+        print(0)
+    else:
+        print(last_owned)
